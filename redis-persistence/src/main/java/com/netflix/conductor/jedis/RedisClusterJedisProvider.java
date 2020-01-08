2e3d35b1bc0dacede06336a77b469b90a340ea3c
Jonathan Baker <jbaker@ccctechcenter.org>
2020-01-07 16:51:23 -0800

    Revert "Merge pull request #17 from mdepak/jedis-cluster"
    This reverts commit 8bb1f6d6d784d78940777818c5e763afc66fd2c3, reversing
    changes made to b12bf398a68f85c15f6d2bf77118c6bc013da616.

--- redis-persistence/src/main/java/com/netflix/conductor/jedis/RedisClusterJedisProvider.java
+++ redis-persistence/src/main/java/com/netflix/conductor/jedis/RedisClusterJedisProvider.java
@@ -14,14 +14,12 @@ package com.netflix.conductor.jedis;
 
 import com.netflix.dyno.connectionpool.Host;
 import com.netflix.dyno.connectionpool.HostSupplier;
-import redis.clients.jedis.JedisShardInfo;
-import redis.clients.jedis.ShardedJedis;
-import redis.clients.jedis.commands.JedisCommands;
-
+import java.util.ArrayList;
 import javax.inject.Inject;
 import javax.inject.Provider;
-
-import java.util.Collections;
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.commands.JedisCommands;
 
 public class RedisClusterJedisProvider implements Provider<JedisCommands> {
 
@@ -34,7 +32,13 @@ public class RedisClusterJedisProvider implements Provider<JedisCommands> {
 
     @Override
     public JedisCommands get() {
-        Host host = hostSupplier.getHosts().get(0);
-        return new ShardedJedis(Collections.singletonList(new JedisShardInfo(host.getHostAddress())));
+        // FIXME This doesn't seem very safe, but is how it was in the code this was moved from.
+        Host host = new ArrayList<>(hostSupplier.getHosts()).get(0);
+        GenericObjectPoolConfig poolConfig = new GenericObjectPoolConfig();
+        poolConfig.setMinIdle(5);
+        poolConfig.setMaxTotal(1000);
+
+        JedisPool jedisPool = new JedisPool(poolConfig, host.getHostName(), host.getPort());
+        return new JedisCluster(jedisPool);
     }
 }
