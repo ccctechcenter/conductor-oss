83b9e77d07ae6c03366d1928ca0c2a85d8bb77d2
sukruthiarjula <sarjula@ccctechcenter.org>
2024-06-11 10:10:33 -0700

    updated the conductor file

--- server/src/main/java/com/netflix/conductor/Conductor.java
+++ server/src/main/java/com/netflix/conductor/Conductor.java
@@ -1,20 +1,22 @@
 /*
  * Copyright 2021 Conductor Authors.
  * <p>
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
  * <p>
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
- * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.netflix.conductor;
 
 import java.io.IOException;
 import java.util.Properties;
-
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -28,14 +30,13 @@ import org.springframework.core.io.FileSystemResource;
 // In case that SQL database is selected this class will be imported back in the appropriate
 // database persistence module.
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
-@ComponentScan(basePackages = {"com.netflix.conductor", "io.orkes.conductor"})
+@ComponentScan(basePackages = {"com.netflix.conductor", "io.orkes.conductor", "org.ccctech.apigateway.conductor"})
 public class Conductor {
 
     private static final Logger log = LoggerFactory.getLogger(Conductor.class);
 
     public static void main(String[] args) throws IOException {
         loadExternalConfig();
-
         SpringApplication.run(Conductor.class, args);
     }
 
